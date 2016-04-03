0e16c6ed83efc37750de2c015328ae57ced9d985
quangable <Nguyen_quan@bentley.edu>
2016-04-03 09:55:25 -0400

    testing

--- app/src/main/java/com/teampet/petsitter/MapsActivity.java
+++ app/src/main/java/com/teampet/petsitter/MapsActivity.java
@@ -173,7 +173,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
     }
-    //testing to make sure I can commit - Quang Nguyen
+    //testing  - Quang Nguyen
     //stop updates
     public void onStop() {
         super.onStop();
