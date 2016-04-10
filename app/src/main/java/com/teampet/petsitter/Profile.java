109117000c968de7945ca78a1585e4fdb8d07475
tmaguire6 <maguiretimo@gmail.com>
2016-04-10 10:40:44 -0400

    Missed some of shivas files the first go around

---
+++ app/src/main/java/com/teampet/petsitter/Profile.java
@@ -0,0 +1,31 @@
+package com.teampet.petsitter;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by Shiv on 4/5/2016.
+ */
+public class Profile extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        // ...
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_profiletest);
+
+    }
+}
---
+++ app/src/main/java/com/teampet/petsitter/TermsConditions.java
@@ -0,0 +1,18 @@
+package com.teampet.petsitter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Shiv on 4/5/2016.
+ */
+public class TermsConditions extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        // ...
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_termsconditionstest);
+
+    }
+
+}
