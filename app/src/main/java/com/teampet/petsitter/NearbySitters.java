347e4c83fe17f60047dc3a5a2101e1a9e34ba6a6
Shiv <golukaul@gmail.com>
2016-04-24 13:00:06 -0400

    nearby sitters tab on menu added

---
+++ app/src/main/java/com/teampet/petsitter/NearbySitters.java
@@ -0,0 +1,16 @@
+package com.teampet.petsitter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Shiv on 4/24/2016.
+ */
+public class NearbySitters extends AppCompatActivity{
+    public void onCreate(Bundle savedInstanceState) {
+        // ...
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_nearbytest);
+
+    }
+}
